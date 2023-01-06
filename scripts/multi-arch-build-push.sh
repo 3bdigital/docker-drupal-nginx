96dbba053c6f46d172e50e88673b7254336c10ca
David Matthews <david@3bweb.com>
2023-01-06 10:48:21 +0000

    Added PHP 8.0, 8.1, 8.2, and updated the build script

--- scripts/multi-arch-build-push.sh
+++ scripts/multi-arch-build-push.sh
@@ -3,10 +3,9 @@
 #################################################
 # Current images
 #################################################
-docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.3 7.3
-docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.3-webroot 7.3-webroot
-docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.4 7.4
-docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.4-webroot 7.4-webroot
+docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:8.0 8.0
+docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:8.1 8.1
+docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:8.2 8.2
 
 #################################################
 # Retired images
@@ -19,3 +18,7 @@ docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupa
 # docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.1-webroot 7.1-webroot
 # docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.2 7.2
 # docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.2-webroot 7.2-webroot
+# docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.3 7.3
+# docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.3-webroot 7.3-webroot
+# docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.4 7.4
+# docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/drupal-nginx:7.4-webroot 7.4-webroot
